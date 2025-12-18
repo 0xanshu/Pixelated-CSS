a66a34dd7d5daaa88ab6120604f0a42db24f128e
anshucodie <priyanshucodie@gmail.com>
2025-12-18 14:03:56 +0530

    fix  turbopack  issue

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 /** @type {import("next").NextConfig} */
 const config = {
   experimental: {
-    serverComponentsExternalPackages: ['@sparticuz/chromium'],
-  },
-  webpack: (config) => {
-    config.externals = [...(config.externals || []), '@sparticuz/chromium'];
-    return config;
+    serverComponentsExternalPackages: ["@sparticuz/chromium"],
   },
 };
 
